40b05fb1170cee2304b26ae97b36baf6947b87a9
Avad3 <65318266+Avad3@users.noreply.github.com>
2022-03-08 23:15:07 -0500

    Update loadingthing.js

--- loadingthing.js
+++ loadingthing.js
@@ -1,6 +1,5 @@
 var div = document.createElement('div')
 div.className = 'div'
-appid.appendChild(div)
 
 var items = document.createElement('center')
 
@@ -16,7 +15,8 @@ proxy.innerText = 'Proxy'
 iframe.setAttribute('onclick', 'javascript: var url = prompt("Enter URL Here (Example: google.com"); document.body.innerHTML = "<iframe src="https://r.einsteinhelp.cf/https://' + url + '" style="position: fixed; top:0; bottom:0; left:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; z-index:999999;">Your Browser Does Not Support</iframe>')
 items.appendChild(proxy)
 
-appid.appendChild(items)
+div.appendChild(items)
+items.appendChild(div)
 
 var style = document.createElement('style')
 style.innerText = '.div{background: darkgray; width: 400px; height: 275px; border-radius: 60px;}.button{cursor: pointer; transition: 0.5s all ease; color: white; background: gray; border-radius: 50px; width: 50px; height: 25px; margin: 5px; text-align: center;}.button:hover{transform: scale(1.15); color: black; background: white;}'
