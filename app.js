620a04e5bd3db3c58b2104beae03ec3f2988a4cb
Avad3 <65318266+Avad3@users.noreply.github.com>
2022-03-12 13:54:58 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+var title = document.createElement("div")
+title.className = "iframeui"
+title.innerText = "IFrame UI"
+appid.appendChild(title)
+
+var items = document.createElement("center")
+
+var tab = document.createElement("button")
+tab.className = "nightmarelol"
+tab.innerText = "IFrame"
+tab.setAttribute("onclick", 'javascript:(function(){var url=prompt("Enter URL Here (example: google.com)");if(url==null){alert("No URL Entered!");}else{document.body.innerHTML = "<iframe src="https://' + (url) + '" style="position:fixed;top:0;bottom:0;left:0;right:0;width:100%;height:100%;border:none;margin:0;padding:0;z-index:999999;">Your browser does not support iframes</iframe>";alert("URL Loaded!");}}())')
+items.appendChild(tab)
+
+var inspect = document.createElement("button")
+inspect.className = "nightmarelol"
+inspect.innerText = "Proxy IFrame"
+inspect.setAttribute("onclick", 'javascript:(function(){var url=prompt("Enter URL Here (example: google.com)");if(url==null){alert("No URL Entered!");}else{document.body.innerHTML = "<iframe src="https://' + (url) + '" style="position:fixed;top:0;bottom:0;left:0;right:0;width:100%;height:100%;border:none;margin:0;padding:0;z-index:999999;">Your browser does not support iframes</iframe>";alert("URL Loaded!");}}())')
+items.appendChild(inspect)
+
+appid.appendChild(items)
+
+var iframestyle = document.createElement("style")
+iframestyle.innerText = ".iframeui {font-size: 60px; text-align: center; color: white; user-select: none; margin-top: 10px;}.nightmarelol{cursor: pointer;background: #303134;border-radius: 45px;height: 50px;text-align: center;line-height: 50px;color: white; border: none; font-size: 18px;user-select: none; margin: 5px;transition: all 0.5s ease} .nightmarelol:hover {transform: scale(1.15);background: white;color: black;}"
+document.getElementsByTagName("html")[0].appendChild(iframestyle)
