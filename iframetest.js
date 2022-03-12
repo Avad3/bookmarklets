fe670d5209a19f36c8d2f476b9fe500c69cb8c0b
Avad3 <65318266+Avad3@users.noreply.github.com>
2022-03-12 14:12:09 -0500

    Update iframetest.js

--- iframetest.js
+++ iframetest.js
@@ -1,1 +1,1 @@
-javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/Avad3/bookmarklets/window.min.js';document.body.appendChild(a);}())
+javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/Avad3/bookmarklets/window.js';document.body.appendChild(a);}())
